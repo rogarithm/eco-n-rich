23dbffcaacf89aba49a677511695256ae48871b0
sehun-kim <rogarithm@gmail.com>
2023-05-16 23:05:31 +0900

    [#1] 사원 정보 조회가 성공하는 케이스를 테스트한다

---
+++ src/test/java/kr/rogarithm/econrich/domain/employee/service/EmployeeServiceTest.java
@@ -0,0 +1,57 @@
+package kr.rogarithm.econrich.domain.employee.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import kr.rogarithm.econrich.domain.employee.dao.EmployeeMapper;
+import kr.rogarithm.econrich.domain.employee.domain.Employee;
+import kr.rogarithm.econrich.domain.employee.dto.EmployeeResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceTest {
+
+    @InjectMocks
+    EmployeeService employeeService;
+
+    @Mock
+    EmployeeMapper employeeMapper;
+
+    @Test
+    public void validIdShouldGetEmployeeInfo() {
+
+        // given
+        Long id = 100L;
+        Employee employee = Employee.builder()
+                                    .id(100L)
+                                    .firstName("Steven")
+                                    .lastName("King")
+                                    .email("SKING")
+                                    .phoneNumber("515.123.4567")
+                                    .hireDate(LocalDate.parse("1987-06-17"))
+                                    .jobId("AD_PRES")
+                                    .salary(BigDecimal.valueOf(24000.00))
+                                    .departmentId(90L)
+                                    .build();
+
+        when(employeeMapper.selectEmployeeById(id))
+                .thenReturn(employee);
+
+        // when
+        EmployeeResponse employeeResponse = employeeService.getEmployeeById(id);
+
+        // then
+        verify(employeeMapper).selectEmployeeById(id);
+        assertNotNull(employeeResponse);
+        assertEquals(id, employeeResponse.getId());
+    }
+
+}
