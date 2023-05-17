5f11da7397747b63d2b9d05922cbdf1e4b15256d
sehun-kim <rogarithm@gmail.com>
2023-05-17 17:49:53 +0900

    [#1] URI에 리소스 정보를 드러낸다

--- src/main/java/kr/rogarithm/econrich/domain/employee/controller/EmployeeController.java
+++ src/main/java/kr/rogarithm/econrich/domain/employee/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import kr.rogarithm.econrich.domain.employee.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
