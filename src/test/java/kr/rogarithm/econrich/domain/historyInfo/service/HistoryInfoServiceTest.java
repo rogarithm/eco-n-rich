eb0c9fa353c92bfced724a20c9280b5ba40e2044
sehun-kim <rogarithm@gmail.com>
2023-05-18 13:43:53 +0900

    [#2] 사원 이력 조회가 성공하는 케이스를 테스트한다

---
+++ src/test/java/kr/rogarithm/econrich/domain/historyInfo/service/HistoryInfoServiceTest.java
@@ -0,0 +1,48 @@
+package kr.rogarithm.econrich.domain.historyInfo.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import kr.rogarithm.econrich.domain.historyInfo.dao.HistoryInfoMapper;
+import kr.rogarithm.econrich.domain.historyInfo.domain.JobHistory;
+import kr.rogarithm.econrich.domain.historyInfo.dto.JobHistoryResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class HistoryInfoServiceTest {
+
+    @InjectMocks
+    HistoryInfoService historyInfoService;
+
+    @Mock
+    HistoryInfoMapper historyInfoMapper;
+
+    @Test
+    public void validIdShouldGetJobHistory() {
+
+        Long id = 101L;
+        JobHistory jobHistory = JobHistory.builder()
+                                          .id(101L)
+                                          .startDate(LocalDate.parse("1989-09-21"))
+                                          .endDate(LocalDate.parse("1993-10-27"))
+                                          .jobId("AC_ACCOUNT")
+                                          .departmentId(110L)
+                                          .build();
+
+        when(historyInfoMapper.selectJobHistoryById(id))
+                .thenReturn(jobHistory);
+        JobHistoryResponse jobHistoryResponse = historyInfoService.getJobHistoryById(id);
+
+        verify(historyInfoMapper).selectJobHistoryById(id);
+        assertNotNull(jobHistoryResponse);
+        assertEquals(id, jobHistoryResponse.getId());
+    }
+
+}
