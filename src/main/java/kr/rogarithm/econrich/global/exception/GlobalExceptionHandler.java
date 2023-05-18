512d32489b49e221e627243570c204e716bc60f8
sehun-kim <rogarithm@gmail.com>
2023-05-18 14:07:41 +0900

    [#2] 사원 이력 조회 요청 실패 케이스를 처리한다

--- src/main/java/kr/rogarithm/econrich/global/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/rogarithm/econrich/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package kr.rogarithm.econrich.global.exception;
 
 import kr.rogarithm.econrich.domain.employee.exception.EmployeeNotFoundException;
+import kr.rogarithm.econrich.domain.historyInfo.exception.JobHistoryNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmployeeNotFoundException.class)
     protected ResponseEntity<Void> employeeNotFoundException(EmployeeNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @ExceptionHandler(JobHistoryNotFoundException.class)
+    protected ResponseEntity<Void> jobHistoryNotFoundException(JobHistoryNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
