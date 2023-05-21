03a9cef973189a690e9eea640382dd12de377635
sehun-kim <rogarithm@gmail.com>
2023-05-22 00:32:01 +0900

    [#6] 직원 한 명의 급여를 인상할 수 있다

--- src/main/java/kr/rogarithm/econrich/domain/department/dao/DepartmentMapper.java
+++ src/main/java/kr/rogarithm/econrich/domain/department/dao/DepartmentMapper.java
@@ -3,9 +3,12 @@ package kr.rogarithm.econrich.domain.department.dao;
 import java.util.List;
 import kr.rogarithm.econrich.domain.employee.domain.JobHistory;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface DepartmentMapper {
 
     List<JobHistory> selectJobHistories(Long departmentId);
+
+    Integer updateEmployeeSalary(@Param("employeeId") Long employeeId, @Param("raiseRate") Double raiseRate);
 }
