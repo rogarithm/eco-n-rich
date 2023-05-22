4db61673f75c0ef4fb6a8017fc024268d3d24a09
sehun-kim <rogarithm@gmail.com>
2023-05-22 12:37:19 +0900

    [#6] raiseSalaryOfDepartment의 모든 쿼리는 하나의 실행 단위로 묶여야 한다

--- src/main/java/kr/rogarithm/econrich/domain/department/service/DepartmentService.java
+++ src/main/java/kr/rogarithm/econrich/domain/department/service/DepartmentService.java
@@ -6,6 +6,7 @@ import kr.rogarithm.econrich.domain.department.exception.InvalidRaiseRateExcepti
 import kr.rogarithm.econrich.domain.employee.domain.JobHistory;
 import kr.rogarithm.econrich.domain.employee.exception.DepartmentNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DepartmentService {
@@ -16,6 +17,7 @@ public class DepartmentService {
         this.departmentMapper = departmentMapper;
     }
 
+    @Transactional
     public Integer raiseSalaryOfDepartment(Long departmentId, Double raiseRate) {
 
         if (raiseRate < 0) {
