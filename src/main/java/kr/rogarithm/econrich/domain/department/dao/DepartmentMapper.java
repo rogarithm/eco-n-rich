00ee4b0828049d8bb705094ff640cb632b3edf10
sehun-kim <rogarithm@gmail.com>
2023-05-22 12:35:57 +0900

    [#6] 테스트 결과를 확인하기 위한 쿼리를 정의한다

--- src/main/java/kr/rogarithm/econrich/domain/department/dao/DepartmentMapper.java
+++ src/main/java/kr/rogarithm/econrich/domain/department/dao/DepartmentMapper.java
@@ -1,5 +1,6 @@
 package kr.rogarithm.econrich.domain.department.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 import kr.rogarithm.econrich.domain.employee.domain.JobHistory;
 import org.apache.ibatis.annotations.Mapper;
@@ -11,4 +12,6 @@ public interface DepartmentMapper {
     List<JobHistory> selectJobHistories(Long departmentId);
 
     Integer updateEmployeeSalary(@Param("employeeId") Long employeeId, @Param("raiseRate") Double raiseRate);
+
+    BigDecimal selectEmployeeSalary(Long employeeId);
 }
